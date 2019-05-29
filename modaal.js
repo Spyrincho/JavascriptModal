dc5d843c495d61c6945bbb427bcf0904f76d2453
Spyrincho <42439682+Spyrincho@users.noreply.github.com>
2019-05-29 02:02:07 +0200

    Update modaal.js

--- modaal.js
+++ modaal.js
@@ -1,32 +1,32 @@
 const modal = {
-  content: document.querySelectorAll('.modaalContent'),
-  buttons: document.querySelectorAll('.modaalKnop'),
+  content: document.querySelectorAll('.modalContent'),
+  buttons: document.querySelectorAll('.modaalButton'),
 
   makeBackground() {
     let background = document.createElement('div');
-    background.classList.add('modaalAchtergrond');
+    background.classList.add('modaalBackground');
     background.addEventListener('click', ()=>this.close());
     return background;
   },
-  makeModal() {
+  makeModaal() {
     let modal = document.createElement('div');
     modal.classList.add('modaal');
     modal.addEventListener('click', (evt)=>evt.stopPropagation());
     return modal;
   },
-  makeCloseButton() {
-    let closeButton = document.createElement('div')
-    closeButton.classList.add('sluitKnop');
-    closeButton.innerHTML = '&#x00D7';
-    closeButton.addEventListener('click', ()=>this.close());
-    return closeButton;
+  makecloseKnop() {
+    let closeKnop = document.createElement('div')
+    closeKnop.classList.add('sluitButton');
+    closeKnop.innerHTML = '&#x00D7';
+    closeKnop.addEventListener('click', ()=>this.close());
+    return closeKnop;
   },
   open(elem) {
     this.background = this.makeBackground();
-    this.closeButton = this.makeCloseButton();
-    this.modal = this.makeModal();
+    this.closeKnop = this.makecloseKnop();
+    this.modal = this.makeModaal();
 
-    this.modal.appendChild(this.closeButton);
+    this.modal.appendChild(this.closeKnop);
     this.modal.appendChild(elem);
     this.background.appendChild(this.modal);
     document.body.appendChild(this.background);
